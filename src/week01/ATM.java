7dcb58f768937c8b5bac23f473ff20446973a150
omaryusufonalan <omaryusufonalan@gmail.com>
2023-09-14 18:08:20 +0300

    ATM Projesi project completed

---
+++ src/week01/ATM.java
@@ -0,0 +1,68 @@
+package week01;
+import java.util.Scanner;
+
+// ATM Projesi
+
+public class ATM {
+    public static void main(String[] args) {
+        String userName, pw;
+        Scanner input = new Scanner(System.in);
+        int right = 3;
+        int balance = 1500;
+        int select;
+
+        while (right > 0) {
+            System.out.print("Kullanıcı Adınız : ");
+            userName = input.nextLine();
+            System.out.print("Parolanız : ");
+            pw = input.nextLine();
+
+            if (userName.equals("patika") && pw.equals("dev123")) {
+                System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
+                do {
+                    System.out.println("1-Para yatırma\n" +
+                            "2-Para Çekme\n" +
+                            "3-Bakiye Sorgula\n" +
+                            "4-Çıkış Yap");
+                    System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz : ");
+                    select = input.nextInt();
+
+                    switch (select) {
+                        case 1:
+                            System.out.print("Para miktarı : ");
+                            int depositAmount = input.nextInt();
+                            balance += depositAmount;
+                            break;
+                        case 2:
+                            System.out.print("Para miktarı : ");
+                            int withdrawAmount = input.nextInt();
+                            if (withdrawAmount > balance) {
+                                System.out.println("Bakiye yetersiz.");
+                            } else {
+                                balance -= withdrawAmount;
+                            }
+                            break;
+                        case 3:
+                            System.out.println("Bakiyeniz : " + balance);
+                            break;
+                        case 4:
+                            System.out.println("Tekrar görüşmek üzere.");
+                            break;
+                        default:
+                            System.out.println("Geçersiz seçim!");
+                            break;
+                    }
+                } while (select != 4);
+                break;
+            } else {
+                right--;
+                System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
+                if (right == 0) {
+                    System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz.");
+                } else {
+                    System.out.println("Kalan Hakkınız : " + right);
+                }
+            }
+        }
+    }
+}
