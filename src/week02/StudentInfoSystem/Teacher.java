3220159d85591eeee867d861180e0429fe1fcd42
omaryusufonalan <omaryusufonalan@gmail.com>
2023-09-17 15:13:01 +0300

    Completed Student Information System project

---
+++ src/week02/StudentInfoSystem/Teacher.java
@@ -0,0 +1,14 @@
+package week02.StudentInfoSystem;
+
+public class Teacher {
+    String name;
+    String mobileNumber;
+    String department;
+
+    public Teacher(String name, String mobileNumber, String department) {
+        this.name = name;
+        this.mobileNumber = mobileNumber;
+        this.department = department;
+    }
+}
+
